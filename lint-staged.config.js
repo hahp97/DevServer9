0753a13ee76979df82254d8daedbb8fb5917afce
huynqq <ibuhuy105@gmail.com>
2023-10-25 11:26:01 +0700

    add husky

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,13 @@
-const path = require('path');
-
-const eslintCommand = (filenames) =>
-  `next lint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
-
-const formatCommand = 'prettier --write';
-const stylelintCommand = 'stylelint --allow-empty-input "**/*.{css,scss}"';
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [formatCommand, eslintCommand],
-  '*.{css,scss}': [formatCommand, stylelintCommand],
-  '!*.{js,jsx,ts,tsx,css,scss}': [formatCommand],
+  // this will check Typescript files
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+  // This will lint and format TypeScript and                                             //JavaScript files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // this will Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) => `yarn prettier --write ${filenames.join(' ')}`,
 };
