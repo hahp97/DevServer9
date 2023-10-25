24d0be7b50e0c27e1b7802d33946d68f02e41cad
huynqq <ibuhuy105@gmail.com>
2023-10-25 11:10:29 +0700

    setup src

---
+++ helper/type.ts
@@ -0,0 +1,22 @@
+type GetEventHandlers<T extends keyof JSX.IntrinsicElements> = Extract<keyof JSX.IntrinsicElements[T], `on${string}`>;
+
+/**
+ * Provides the event type for a given element and handler.
+ *
+ * @example
+ *
+ * type MyEvent = EventFor<"input", "onChange">;
+ */
+export type EventFor<
+  TElement extends keyof JSX.IntrinsicElements,
+  THandler extends GetEventHandlers<TElement>,
+> = JSX.IntrinsicElements[TElement][THandler] extends ((e: infer TEvent) => any) | undefined ? TEvent : never;
+
+/**
+ * Provides the might be something or null or undefined.
+ *
+ * @example
+ *
+ * type phoneNumber: Maybe<string>;
+ */
+export type Maybe<T> = T | null | undefined;
