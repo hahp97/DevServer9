4824d39dcf4363f1b9ba74bd3b9b0ddf2eb0b40b
huynqq <ibuhuy105@gmail.com>
2023-10-26 13:08:22 +0700

    add libary

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config, import { radixThemePreset } from 'radix-themes-tw';} */
+
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
